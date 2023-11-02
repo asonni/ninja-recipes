12694fc23d9191cf87b02c18ab8a95657280d94a
Aladdin Sonni <aladdin.sonni@gmail.com>
2023-11-02 11:12:57 +0200

    Fix loading skeleton mobile responsive issue

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,7 @@ import { SkeletonCard } from '@/components/skeleton-card';
 const Loading = () => {
   return (
     <main>
-      <div className="grid grid-cols-3 gap-8">
+      <div className="mx-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:mx-0 lg:grid-cols-3">
         {'abcdefghi'.split('').map(i => (
           <SkeletonCard key={i} />
         ))}
